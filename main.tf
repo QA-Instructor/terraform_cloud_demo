28936bccdbc35e634918e15df670e69ac1b1a2a0
Victoria Lloyd <QAInstructorSky@gmail.com>
2024-12-10 15:00:25 +0000

    Outputs created

--- main.tf
+++ main.tf
@@ -130,15 +130,6 @@ resource "random_password" "password" {
 }
 
 
-
-output "appgw_public_ip_address" {
-  description = "The public IP address of Application Gateway."
-  value       = azurerm_public_ip.pubip.ip_address
-}
-
-
-
-
 resource "azurerm_windows_virtual_machine" "VM" {
   for_each = toset(["VM1", "VM2"])
   name                = each.value 
---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "appgw_public_ip_address" {
+  description = "The public IP address of Application Gateway."
+  value       = azurerm_public_ip.pubip.ip_address
+}
+
+output "vm_names" {
+  description = "The names of the VMs."
+  value       = azurerm_windows_virtual_machine.*.name
+}
