92184d2299a049148161b5e859a51429ace51ad6
Victoria Lloyd <QAInstructorSky@gmail.com>
2024-12-10 15:01:36 +0000

    Outputs edited

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@ output "appgw_public_ip_address" {
   value       = azurerm_public_ip.pubip.ip_address
 }
 
-output "vm_names" {
-  description = "The names of the VMs."
-  value       = azurerm_windows_virtual_machine.*.name
-}
+# output "vm_names" {
+#   description = "The names of the VMs."
+#   value       = azurerm_windows_virtual_machine.*.name
+# }
